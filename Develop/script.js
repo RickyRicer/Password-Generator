ed76916a12d3e0dcca88f75cdfcaf2a2410209b1
Rick Ansay <rick.ansay@gmail.com>
2021-11-03 22:27:16 -0600

    Added randomizer, prompts for selecting pw length, and started confirm coding

--- Develop/script.js
+++ Develop/script.js
@@ -1,13 +1,48 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// Generator functions
 
-function getRanmdomLowercase() {
+
+// Random Generator Functions
+// Random Lowercase
+function getRandomLower() {
   return String.fromCharCode(Math.floor(Math.random() *26) + 97);
 }
 
-console.log(getRandomLower());
+// Random Uppercase
+function getRandomUpper() {
+  return String.fromCharCode(Math.floor(Math.random() *26) + 65);
+}
+
+// Random Numbers
+function getRandomNumber() {
+  return String.fromCharCode(Math.floor(Math.random() * 10) + 48)
+}
+
+//Random Symbols
+function getRandomSymbol() {
+  const symbols = '!"#$%&()*+,-./:;<=>?@[\]^_{|}~';
+  return symbols[Math.floor(Math.random() * symbols.length)];
+}
+
+console.log(getRandomUpper());
+
+// Prompt Password Length
+
+let answer = prompt('Please select a length between 8 and 128');
+let response;
+
+if(answer.length === 0){
+  response = 'You must enter a number between 8 and 128';
+} else if(answer.length > 8){
+  response = 'You must enter a number between 8 and 128';
+} else if (answer.length < 128) {
+  response = 'You must enter a number between 8 and 128';
+}
+
+alert(response);
+
+let yesLower = confirm(`Would you like to include lower case letters?`)
 
 function generatePassword(){
   console.log("Generating password...");
