386f85fc7164624092cef1f32a46e685de31f824
Rick Ansay <rick.ansay@gmail.com>
2021-11-04 22:31:38 -0600

    More randomizer functions

--- Develop/script.js
+++ Develop/script.js
@@ -3,79 +3,102 @@ var generateBtn = document.querySelector("#generate");
 
 
 
+// Prompt Password Length
+
+let answer = prompt('Please select a character length between 8 and 128');
+let response;
+
+if(answer.length === 0){
+  response = 'You must enter a number between 8 and 128';
+} else if(answer.length < 8){
+  response = 'You must enter a number between 8 and 128';
+} else if (answer.length > 128) {
+  response = 'You must enter a number between 8 and 128';
+}
+
+alert(response);
+
+
+// Password Selectors
+
+let lower = confirm(`Would you like to include lowercase letters?`)
+let upper = confirm('Would you like to include uppercase letters?')
+let number = confirm('Would you like to include numbers?')
+let symbol = confirm('Would you like to include symbols?')
+
+function generatePassword(lower, upper, number, symbol, length) {
+  let generatedPassword = '';
+  
+  const typesCount = lower + upper + number + symbol;
+  
+  console.log('typesCount: ', typesCount);
+  
+  const typesArr = [{ lower }, { upper }, { number }, { symbol },].filter (
+    item => Object.values(item)[0]
+    );
+    
+    console.log('typesArr: ', typesArr);
+    if(typesCount === 0) {
+      return '';
+    }
+    
+    for(let i = 0; i < length; i += typesCount) {
+      typesArr.forEach(type =>{
+        const funcName = Object.keys(type)[0];
+        
+        generatedPassword += randomFunc[funcName]();
+      });
+    }
+    
+    const yourPassword = generatedPassword
+  }
+  
+  // Write password to the #password input
+  function writePassword() {
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
+    
+    passwordText.value = password;
+    
+  }
+  
+  // Add event listener to generate button
+  generateBtn.addEventListener("click", writePassword);
+  
+  /*var length = 10,
+  charset = "ABCDEFGHIJKLMNOPQRSTUVWXY",
+  retVal = "";
+  return retVal;
+*/
+
 // Random Generator Functions
+
+const randomFunc = {
+  lower: randomLower,
+  upper: randomUpper,
+  number: randomNumber,
+  symbols: randomSymbol,
+}
+
 // Random Lowercase
-function getRandomLower() {
+function randomLower() {
   return String.fromCharCode(Math.floor(Math.random() *26) + 97);
 }
 
 // Random Uppercase
-function getRandomUpper() {
+function randomUpper() {
   return String.fromCharCode(Math.floor(Math.random() *26) + 65);
 }
 
 // Random Numbers
-function getRandomNumber() {
+function randomNumber() {
   return String.fromCharCode(Math.floor(Math.random() * 10) + 48)
 }
 
 //Random Symbols
-function getRandomSymbol() {
+function randomSymbol() {
   const symbols = '!"#$%&()*+,-./:;<=>?@[\]^_{|}~';
   return symbols[Math.floor(Math.random() * symbols.length)];
 }
 
-console.log(getRandomUpper());
-
-// Prompt Password Length
-
-// let answer = prompt('Please select a character length between 8 and 128');
-//let response;
-//
-//if(answer.length === 0){
-//  response = 'You must enter a number between 8 and 128';
-//} else if(answer.length < 8){
-//  response = 'You must enter a number between 8 and 128';
-//} else if (answer.length > 128) {
-//  response = 'You must enter a number between 8 and 128';
-//}
-
-//alert(response);
-
-let answer = prompt('Please enter a character length between 8 and 128').length;
-
-let responses = {
-more: 'You must enter a number between 8 and 128',
-less: 'You must enter a number between 8 and 128',
-default: 'You didn\'t answer?'
-}
-
-alert(answer == 0 && responses.default || answer < 8 && responses.less || answer > 128 && responses.more);
-
-// Password Selectors
-
-let yesLower = confirm(`Would you like to include lowercase letters?`)
-let yesUpper = confirm('Would you like to include uppercase letters?')
-let yesNumber = confirm('Would you like to include numbers?')
-let yesSymbol = confirm('Would you like to include symbols?')
-
-function generatePassword(){
-  console.log("Generating password...");
-  
-  var length = 10,
-      charset = "ABCDEFGHIJKLMNOPQRSTUVWXY",
-      retVal = "";
-  return retVal;
-}
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-}
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+console.log(randomUpper());
